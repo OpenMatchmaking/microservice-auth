d44a94a26d6723a07d04d8e7a704bf521130f521
Valeryi Savich <Relrin78@gmail.com>
2018-02-15 14:49:38 +0300

    Added options for running the server

--- auth/app/commands/runserver.py
+++ auth/app/commands/runserver.py
@@ -1,5 +1,5 @@
-import os
-from sanic_script import Command
+from sanic_script import Command, Option
+
 from app import app
 
 
@@ -9,11 +9,19 @@ class RunServerCommand(Command):
     """
     app = app
 
+    option_list = (
+        Option('--host', '-h', dest='host', default='0.0.0.0'),
+        Option('--port', '-p', dest='port', default=8000),
+        Option('--debug', '-d', dest='debug', default=False),
+        Option('--ssl', '-s', dest='ssl', default=None),
+        Option('--workers', '-w', dest='workers', default=1),
+    )
+
     def run(self, *args, **kwargs):
         self.app.run(
-            host=kwargs.get('host', os.environ.get("APP_HOST", None)),
-            port=kwargs.get('port', os.environ.get("APP_PORT", None)),
-            debug=kwargs.get('debug', os.environ.get("APP_DEBUG", False)),
-            ssl=kwargs.get('ssl', os.environ.get("APP_SSL", None)),
-            workers=kwargs.get('workers', os.environ.get("APP_WORKERS", 1)),
+            host=kwargs.get('host', self.app.config["APP_HOST"]),
+            port=kwargs.get('port', self.app.config["APP_PORT"]),
+            debug=kwargs.get('debug', self.app.config["APP_DEBUG"]),
+            ssl=kwargs.get('ssl', self.app.config["APP_SSL"]),
+            workers=kwargs.get('workers', self.app.config["APP_WORKERS"]),
         )
