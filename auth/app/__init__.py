2e6804da57d1f985f84b341cf9ca0548177342bb
Valeryi Savich <Relrin78@gmail.com>
2018-02-15 14:52:37 +0300

    Implemented aioredis with test endpoint

--- auth/app/__init__.py
+++ auth/app/__init__.py
@@ -1,4 +1,16 @@
 from sanic import Sanic
 
+from app.extensions.redis import RedisExtension
+
 
 app = Sanic('microservice-auth')
+app.redis = RedisExtension(app)
+
+
+from sanic import response
+@app.route("/")
+async def handle(request):
+    async with request.app.redis_pool.get() as redis:
+        await redis.set('test-my-key', 'value')
+        val = await redis.get('test-my-key')
+    return response.text(val.decode('utf-8'))
