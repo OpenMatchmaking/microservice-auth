7240ae6f6d118cfa49e370afde5bcc3df928325d
Valeryi Savich <relrin78@gmail.com>
2018-02-18 13:47:25 +0300

    Implemented health check for API Gateways

--- auth/app/__init__.py
+++ auth/app/__init__.py
@@ -1,4 +1,5 @@
 from sanic import Sanic
+from sanic.response import text
 
 from app.extensions.redis import RedisExtension
 from app.extensions.mongodb import MongoDbExtension
@@ -12,4 +13,9 @@ RedisExtension(app)
 MongoDbExtension(app)
 
 # Public API
+async def heatlh_check(request):
+    return text('OK')
+
+
 app.blueprint(users_bp_v1)
+app.add_route(heatlh_check, '/auth/api/health-check', methods=['GET', ])
