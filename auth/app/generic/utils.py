f8c3a86739b3bdfca3ecb75f8d9cdaf276d15479
Valeryi Savich <relrin78@gmail.com>
2018-02-19 23:04:26 +0300

    Implemented processing strings without a period

--- auth/app/generic/utils.py
+++ auth/app/generic/utils.py
@@ -2,4 +2,6 @@ ERROR_FIELD_NAME = 'details'
 
 
 def wrap_error(message):
+    if isinstance(message, str) and not message.endswith('.'):
+        message = message + '.'
     return {ERROR_FIELD_NAME: message}
