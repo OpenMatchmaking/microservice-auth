dc8a4ec9a37b4ba9b43161128cf8c35ce104b561
Valeryi Savich <relrin78@gmail.com>
2018-03-06 10:20:07 +0300

    Updated run_server.py command

--- auth/app/commands/run_server.py
+++ auth/app/commands/run_server.py
@@ -16,8 +16,8 @@ class RunServerCommand(Command):
 
     def run(self, *args, **kwargs):
         self.app.run(
-            host=kwargs.get('host', self.app.config["APP_HOST"]),
-            port=kwargs.get('port', self.app.config["APP_PORT"]),
+            host=kwargs.get('host', None) or self.app.config["APP_HOST"],
+            port=kwargs.get('port', None) or self.app.config["APP_PORT"],
             debug=self.app.config["APP_DEBUG"],
             ssl=self.app.config["APP_SSL"],
             workers=self.app.config["APP_WORKERS"],
