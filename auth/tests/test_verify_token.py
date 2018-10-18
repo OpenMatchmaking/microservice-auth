63cc65fb20b19eb73a42ce87f26df1beaae4d634
Valeryi Savich <relrin78@gmail.com>
2018-10-18 16:56:36 +0300

    Implemented tests for the new verify JSON Web Token endpoint

--- auth/tests/test_verify_token.py
+++ auth/tests/test_verify_token.py
@@ -1,10 +1,20 @@
-import json
-
 from freezegun import freeze_time
-from sage_utils.constants import HEADER_ERROR, AUTHORIZATION_ERROR, TOKEN_ERROR
+from sage_utils.amqp.clients import RpcAmqpClient
+from sage_utils.constants import VALIDATION_ERROR, TOKEN_ERROR
 from sage_utils.wrappers import Response
 
+from app.token.api.workers.generate_token import GenerateTokenWorker
+from app.token.api.workers.verify_token import VerifyTokenWorker
 from app.users.documents import User
+
+
+REQUEST_TOKEN_QUEUE = GenerateTokenWorker.QUEUE_NAME
+REQUEST_TOKEN_EXCHANGE = GenerateTokenWorker.REQUEST_EXCHANGE_NAME
+RESPONSE_TOKEN_EXCHANGE = GenerateTokenWorker.RESPONSE_EXCHANGE_NAME
+
+REQUEST_QUEUE = VerifyTokenWorker.QUEUE_NAME
+REQUEST_EXCHANGE = VerifyTokenWorker.REQUEST_EXCHANGE_NAME
+RESPONSE_EXCHANGE = VerifyTokenWorker.RESPONSE_EXCHANGE_NAME
 
 
 async def test_verify_token_returns_is_valid_for_correct_access_token(sanic_server):
@@ -12,139 +22,77 @@ async def test_verify_token_returns_is_valid_for_correct_access_token(sanic_serv
     user = User(**{"username": "user", "password": "123456"})
     await user.commit()
 
-    url = sanic_server.app.url_for('json-web-token-api.create-token')
-    payload = json.dumps({
+    payload = {
         "username": "user",
         "password": "123456"
-    })
-    response = await sanic_server.post(url, data=payload)
-    tokens = await response.json()
-    assert response.status == 201
-    assert len(tokens.keys()) == 2
-    assert sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME'] in tokens.keys()
-    assert sanic_server.app.config['JWT_REFRESH_TOKEN_FIELD_NAME'] in tokens.keys()
-
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    header_token = "{} {}".format(
-        sanic_server.app.config['JWT_AUTHORIZATION_HEADER_PREFIX'],
-        tokens["access_token"]
+    }
+    client = RpcAmqpClient(
+        sanic_server.app,
+        routing_key=REQUEST_TOKEN_QUEUE,
+        request_exchange=REQUEST_TOKEN_EXCHANGE,
+        response_queue='',
+        response_exchange=RESPONSE_TOKEN_EXCHANGE
     )
-    headers = {sanic_server.app.config['JWT_AUTHORIZATION_HEADER_NAME']: header_token}
-    response = await sanic_server.post(url, headers=headers)
-    response_json = await response.json()
-    assert response.status == 200
+    response = await client.send(payload=payload)
 
-    assert 'is_valid' in response_json.keys()
-    assert response_json['is_valid'] == True
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME in response.keys()
+    tokens = response[Response.CONTENT_FIELD_NAME]
 
-    assert Response.CONTENT_FIELD_NAME in response_json.keys()
-    assert response_json[Response.CONTENT_FIELD_NAME] == "OK"
-
-    await User.collection.delete_one({'id': user.id})
-
-
-async def test_verify_token_return_error_for_a_missing_authorization_header(sanic_server):
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    response = await sanic_server.post(url)
-    response_json = await response.json()
-    assert response.status == 400
-    assert len(response_json.keys()) == 2
-    assert 'is_valid' in response_json.keys()
-    assert response_json['is_valid'] is False
-
-    assert Response.ERROR_FIELD_NAME in response_json.keys()
-    assert Response.EVENT_FIELD_NAME not in response_json.keys()
-    assert Response.CONTENT_FIELD_NAME not in response_json.keys()
-    error = response_json[Response.ERROR_FIELD_NAME]
-
-    assert Response.ERROR_TYPE_FIELD_NAME in error.keys()
-    assert error[Response.ERROR_TYPE_FIELD_NAME] == AUTHORIZATION_ERROR
-
-    assert Response.ERROR_DETAILS_FIELD_NAME in error.keys()
-    assert error[Response.ERROR_DETAILS_FIELD_NAME] == "Authorization header isn't set in request."
-
-
-async def test_verify_token_return_error_for_a_missing_header_prefix(sanic_server):
-    await User.collection.delete_many({})
-    user = User(**{"username": "user", "password": "123456"})
-    await user.commit()
-
-    url = sanic_server.app.url_for('json-web-token-api.create-token')
-    payload = json.dumps({
-        "username": "user",
-        "password": "123456"
-    })
-    response = await sanic_server.post(url, data=payload)
-    tokens = await response.json()
-    assert response.status == 201
     assert len(tokens.keys()) == 2
     assert sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME'] in tokens.keys()
     assert sanic_server.app.config['JWT_REFRESH_TOKEN_FIELD_NAME'] in tokens.keys()
 
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    headers = {sanic_server.app.config['JWT_AUTHORIZATION_HEADER_NAME']: tokens["access_token"]}
-    response = await sanic_server.post(url, headers=headers)
-    response_json = await response.json()
-    assert response.status == 400
-    assert len(response_json.keys()) == 2
-    assert 'is_valid' in response_json.keys()
-    assert response_json['is_valid'] is False
+    verify_payload = {
+        sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME']: tokens['access_token']
+    }
+    client = RpcAmqpClient(
+        sanic_server.app,
+        routing_key=REQUEST_QUEUE,
+        request_exchange=REQUEST_EXCHANGE,
+        response_queue='',
+        response_exchange=RESPONSE_EXCHANGE
+    )
+    response = await client.send(payload=verify_payload)
 
-    assert Response.ERROR_FIELD_NAME in response_json.keys()
-    assert Response.EVENT_FIELD_NAME not in response_json.keys()
-    assert Response.CONTENT_FIELD_NAME not in response_json.keys()
-    error = response_json[Response.ERROR_FIELD_NAME]
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME in response.keys()
+    content = response[Response.CONTENT_FIELD_NAME]
 
-    assert Response.ERROR_TYPE_FIELD_NAME in error.keys()
-    assert error[Response.ERROR_TYPE_FIELD_NAME] == HEADER_ERROR
-
-    assert Response.ERROR_DETAILS_FIELD_NAME in error.keys()
-    assert error[Response.ERROR_DETAILS_FIELD_NAME] == 'Before the token necessary to ' \
-                                                       'specify the `JWT` prefix.'
+    assert 'is_valid' in content.keys()
+    assert content['is_valid'] is True
 
     await User.collection.delete_one({'id': user.id})
 
 
-async def test_verify_token_return_error_for_an_invalid_header_prefix(sanic_server):
-    await User.collection.delete_many({})
-    user = User(**{"username": "user", "password": "123456"})
-    await user.commit()
+async def test_verify_token_return_a_validation_error_for_a_missing_access_token(sanic_server):
+    client = RpcAmqpClient(
+        sanic_server.app,
+        routing_key=REQUEST_QUEUE,
+        request_exchange=REQUEST_EXCHANGE,
+        response_queue='',
+        response_exchange=RESPONSE_EXCHANGE
+    )
+    response = await client.send(payload={})
 
-    url = sanic_server.app.url_for('json-web-token-api.create-token')
-    payload = json.dumps({
-        "username": "user",
-        "password": "123456"
-    })
-    response = await sanic_server.post(url, data=payload)
-    tokens = await response.json()
-    assert response.status == 201
-    assert len(tokens.keys()) == 2
-    assert sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME'] in tokens.keys()
-    assert sanic_server.app.config['JWT_REFRESH_TOKEN_FIELD_NAME'] in tokens.keys()
+    assert Response.ERROR_FIELD_NAME in response.keys()
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME not in response.keys()
 
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    header_token = "NOT_JWT {}".format(tokens["access_token"])
-    headers = {sanic_server.app.config['JWT_AUTHORIZATION_HEADER_NAME']: header_token}
-    response = await sanic_server.post(url, headers=headers)
-    response_json = await response.json()
-    assert response.status == 400
-    assert len(response_json.keys()) == 2
-    assert 'is_valid' in response_json.keys()
-    assert response_json['is_valid'] is False
+    errors = response[Response.ERROR_FIELD_NAME]
+    assert len(errors.keys()) == 2
+    assert 'is_valid' not in errors.keys()
 
-    assert Response.ERROR_FIELD_NAME in response_json.keys()
-    assert Response.EVENT_FIELD_NAME not in response_json.keys()
-    assert Response.CONTENT_FIELD_NAME not in response_json.keys()
-    error = response_json[Response.ERROR_FIELD_NAME]
+    assert Response.ERROR_TYPE_FIELD_NAME in errors.keys()
+    assert errors[Response.ERROR_TYPE_FIELD_NAME] == VALIDATION_ERROR
 
-    assert Response.ERROR_TYPE_FIELD_NAME in error.keys()
-    assert error[Response.ERROR_TYPE_FIELD_NAME] == HEADER_ERROR
+    assert Response.ERROR_DETAILS_FIELD_NAME in errors.keys()
 
-    assert Response.ERROR_DETAILS_FIELD_NAME in error.keys()
-    assert error[Response.ERROR_DETAILS_FIELD_NAME] == 'Before the token necessary to ' \
-                                                       'specify the `JWT` prefix.'
-
-    await User.collection.delete_one({'id': user.id})
+    access_token_field = sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME']
+    assert access_token_field in errors[Response.ERROR_DETAILS_FIELD_NAME]
+    assert len(errors[Response.ERROR_DETAILS_FIELD_NAME][access_token_field]) == 1
+    assert errors[Response.ERROR_DETAILS_FIELD_NAME][access_token_field][0] == 'Missing data for ' \
+                                                                               'required field.'
 
 
 async def test_verify_token_return_error_for_expired_access_token(sanic_server):
@@ -152,38 +100,45 @@ async def test_verify_token_return_error_for_expired_access_token(sanic_server):
     user = User(**{"username": "user", "password": "123456"})
     await user.commit()
 
-    url = sanic_server.app.url_for('json-web-token-api.create-token')
-    payload = json.dumps({
+    payload = {
         "username": "user",
         "password": "123456"
-    })
-
+    }
     with freeze_time("2000-01-01"):
-        response = await sanic_server.post(url, data=payload)
-        tokens = await response.json()
+        client = RpcAmqpClient(
+            sanic_server.app,
+            routing_key=REQUEST_TOKEN_QUEUE,
+            request_exchange=REQUEST_TOKEN_EXCHANGE,
+            response_queue='',
+            response_exchange=RESPONSE_TOKEN_EXCHANGE
+        )
+        response = await client.send(payload=payload)
 
-    assert response.status == 201
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME in response.keys()
+    tokens = response[Response.CONTENT_FIELD_NAME]
+
     assert len(tokens.keys()) == 2
     assert sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME'] in tokens.keys()
     assert sanic_server.app.config['JWT_REFRESH_TOKEN_FIELD_NAME'] in tokens.keys()
 
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    header_token = "{} {}".format(
-        sanic_server.app.config['JWT_AUTHORIZATION_HEADER_PREFIX'],
-        tokens["access_token"]
+    verify_payload = {
+        sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME']: tokens['access_token']
+    }
+    client = RpcAmqpClient(
+        sanic_server.app,
+        routing_key=REQUEST_QUEUE,
+        request_exchange=REQUEST_EXCHANGE,
+        response_queue='',
+        response_exchange=RESPONSE_EXCHANGE
     )
-    headers = {sanic_server.app.config['JWT_AUTHORIZATION_HEADER_NAME']: header_token}
-    response = await sanic_server.post(url, headers=headers)
-    response_json = await response.json()
-    assert response.status == 400
-    assert len(response_json.keys()) == 2
-    assert 'is_valid' in response_json.keys()
-    assert response_json['is_valid'] is False
+    response = await client.send(payload=verify_payload)
 
-    assert Response.ERROR_FIELD_NAME in response_json.keys()
-    assert Response.EVENT_FIELD_NAME not in response_json.keys()
-    assert Response.CONTENT_FIELD_NAME not in response_json.keys()
-    error = response_json[Response.ERROR_FIELD_NAME]
+    assert len(response.keys()) == 2
+    assert Response.ERROR_FIELD_NAME in response.keys()
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME not in response.keys()
+    error = response[Response.ERROR_FIELD_NAME]
 
     assert Response.ERROR_TYPE_FIELD_NAME in error.keys()
     assert error[Response.ERROR_TYPE_FIELD_NAME] == TOKEN_ERROR
@@ -199,36 +154,45 @@ async def test_verify_token_return_error_for_invalid_access_token(sanic_server):
     user = User(**{"username": "user", "password": "123456"})
     await user.commit()
 
-    url = sanic_server.app.url_for('json-web-token-api.create-token')
-    payload = json.dumps({
+    payload = {
         "username": "user",
         "password": "123456"
-    })
-    response = await sanic_server.post(url, data=payload)
-    tokens = await response.json()
-    assert response.status == 201
+    }
+    client = RpcAmqpClient(
+        sanic_server.app,
+        routing_key=REQUEST_TOKEN_QUEUE,
+        request_exchange=REQUEST_TOKEN_EXCHANGE,
+        response_queue='',
+        response_exchange=RESPONSE_TOKEN_EXCHANGE
+    )
+    response = await client.send(payload=payload)
+
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME in response.keys()
+    tokens = response[Response.CONTENT_FIELD_NAME]
+
     assert len(tokens.keys()) == 2
     assert sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME'] in tokens.keys()
     assert sanic_server.app.config['JWT_REFRESH_TOKEN_FIELD_NAME'] in tokens.keys()
 
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    header_token = "{} {}".format(
-        sanic_server.app.config['JWT_AUTHORIZATION_HEADER_PREFIX'],
-        tokens["access_token"][:-1]
+    verify_payload = {
+        sanic_server.app.config['JWT_ACCESS_TOKEN_FIELD_NAME']: tokens['access_token'][:-1]
+    }
+    client = RpcAmqpClient(
+        sanic_server.app,
+        routing_key=REQUEST_QUEUE,
+        request_exchange=REQUEST_EXCHANGE,
+        response_queue='',
+        response_exchange=RESPONSE_EXCHANGE
     )
-    headers = {sanic_server.app.config['JWT_AUTHORIZATION_HEADER_NAME']: header_token}
-    response = await sanic_server.post(url, headers=headers)
-    response_json = await response.json()
-    assert response.status == 400
-    assert len(response_json.keys()) == 2
-    assert 'is_valid' in response_json.keys()
-    assert response_json['is_valid'] is False
+    response = await client.send(payload=verify_payload)
 
-    assert Response.ERROR_FIELD_NAME in response_json.keys()
-    assert Response.EVENT_FIELD_NAME not in response_json.keys()
-    assert Response.CONTENT_FIELD_NAME not in response_json.keys()
-    error = response_json[Response.ERROR_FIELD_NAME]
+    assert len(response.keys()) == 2
+    assert Response.ERROR_FIELD_NAME in response.keys()
+    assert Response.EVENT_FIELD_NAME in response.keys()
+    assert Response.CONTENT_FIELD_NAME not in response.keys()
 
+    error = response[Response.ERROR_FIELD_NAME]
     assert Response.ERROR_TYPE_FIELD_NAME in error.keys()
     assert error[Response.ERROR_TYPE_FIELD_NAME] == TOKEN_ERROR
 
@@ -236,27 +200,3 @@ async def test_verify_token_return_error_for_invalid_access_token(sanic_server):
     assert error[Response.ERROR_DETAILS_FIELD_NAME] == 'Signature verification failed.'
 
     await User.collection.delete_one({'id': user.id})
-
-
-async def test_verify_token_get_not_allowed(sanic_server):
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    response = await sanic_server.get(url)
-    assert response.status == 405
-
-
-async def test_verify_token_patch_not_allowed(sanic_server):
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    response = await sanic_server.patch(url)
-    assert response.status == 405
-
-
-async def test_verify_token_put_not_allowed(sanic_server):
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    response = await sanic_server.put(url)
-    assert response.status == 405
-
-
-async def test_verify_token_delete_not_allowed(sanic_server):
-    url = sanic_server.app.url_for('json-web-token-api.verify-token')
-    response = await sanic_server.delete(url)
-    assert response.status == 405
